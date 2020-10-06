67654f0bdac0c048ad805db55f165486090bb3e4
Benjamin Dawson <ben@Zolv.com>
2020-10-06 22:37:11 +0100

    added font group

---
+++ src/Components/Groups/Font/Font.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Text from "../../Shapes/Text";
+import Block from "../../Shapes/Block";
+import "./Font.scss";
+
+function Font() {
+    return (
+        <div className="font parent">
+            <Block />
+            <Block />
+            <Text text="A" amount={24} />
+        </div>
+    )
+}
+
+export default Font
--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import "./Hero.scss";
-import Demo from "./Groups/Demo/Demo";
+import Font from "./Groups/Font/Font";
 function Hero() {
 
     return (
@@ -11,7 +11,7 @@ function Hero() {
             </div>
 
             <div className="ortho">
-                <Demo />
+                <Font />
                 {/* stuff slowly floats? */}
                 {/* rolling balls around content, behind and infront? - if hero slides right */}
                 {/* cup, calender */}
