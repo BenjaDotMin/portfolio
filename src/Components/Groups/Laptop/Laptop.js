5ee129b3f9f22ab554d6d1e00667a360e3daef08
Benjamin Dawson <ben@Zolv.com>
2020-10-09 18:27:51 +0100

    example added for gsap timeline toggle

--- src/Components/Groups/Laptop/Laptop.js
+++ src/Components/Groups/Laptop/Laptop.js
@@ -1,24 +1,31 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import Cylinder from "../../Shapes/Cylinder";
 import Block from "../../Shapes/Block";
 import gsap from "gsap";
 import "./Laptop.scss";
 
 function Laptop() {
-
-    const laptopTop = useRef(null)
+    const laptopTop = useMemo(() => gsap.timeline({ paused: true }), []); //create timeline on init
     const [laptopOpen, setLaptopOpen] = useState(false);
 
-    const openLaptop = () => (
-        gsap.fromTo(".laptop__top", { rotateX: -180 }, { duration: 1.5, rotateX: -70, ease: "sine.inOut" })
-    )
-
+    //add to timeline after render, run only once with []
     useEffect(() => {
+        laptopTop.to(".laptop__top", { duration: 1.5, rotateX: 290, ease: "sine.inOut" })
+    }, []);
 
-    }, [laptopTop, gsap])
+    //another, run every time var changes
+    useEffect(() => {
+        if (laptopOpen) {
+            laptopTop.play();
+
+        } else {
+            laptopTop.reverse()
+        }
+    }, [laptopOpen]);
+
     return (
-        <div className="laptop parent" onClick={openLaptop}>
-            <Block className="laptop__top" ref={laptopTop} />
+        <div className="laptop parent" onClick={() => setLaptopOpen(!laptopOpen)}>
+            <Block className="laptop__top" />
             <Block className="laptop__bottom" />
             <Cylinder clickable className="laptop__button" />
         </div>
